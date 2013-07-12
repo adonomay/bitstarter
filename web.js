5874f4e197f4b3b3baa7bda8547fe54d26bf9d1a
Adam Gardner <adam.s.gardner10@gmail.com>
2013-07-12 01:02:26 +0000

    read content from index.html

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var fs = require('fs')n
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var contents = fs.readFileSync('index.html');
+  var contentsAsString = contents.toString();
   response.send('Hello World 2!');
 });
 
